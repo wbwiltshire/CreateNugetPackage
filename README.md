2f202d64c961a9413d361498fcbb27cc4fc516fe
Triplesticks <Triplesticks62@gmail.com>
2017-05-12 14:38:24 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,10 @@
+Nuget Package Creation Targeting Multiple Frameworks
+===
+
+Create a Nuget package targeting multiple frameworks (e.g. Core, Standard, and Legacy Framework)
+* Example using a separate .nuspec file done manually
+* Example using VS 2017 done automatically
+
+### Links
+
+[Targeting Multiple .NET Framework in a single Nuget Package](https://www.bartwolff.com/Blog/2017/03/20/targeting-multiple-net-platforms-in-a-single-nuget-package-w)
---
+++ VSPackage/MathLib/Math.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MathLib
+{
+    public class Math
+    {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+}
